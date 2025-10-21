8575d9c49521dc22fa08a204eee1f9b791e16b03
JFelipex3 <jfelipe.machuca@gmail.com>
2025-10-21 18:49:45 -0300

    feat: mejorar el reducer de tareas con lógica de gestión de estado y acciones

--- src/05-useReducer/reducer/taskReducer.ts
+++ src/05-useReducer/reducer/taskReducer.ts
@@ -11,14 +11,59 @@ interface TaskState {
     pending: number;
 }
 
-export type TaskAction = 
-| { type: 'ADD_TODO', payload: string }
-| { type: 'TOGGLE_TODO', payload: number}
-| { type: 'DELETE_TODO', payload: number};
+export type TaskAction =
+    | { type: 'ADD_TODO', payload: string }
+    | { type: 'TOGGLE_TODO', payload: number }
+    | { type: 'DELETE_TODO', payload: number };
 
 export const taskReducer = (state: TaskState, action: TaskAction): TaskState => {
 
-    
+    switch (action.type) {
+        case 'ADD_TODO': {
+            const newTodo: Todo = {
+                id: Date.now(),
+                text: action.payload,
+                completed: false
+            };
 
-    return state;
-}
+            return {
+                ...state,
+                todos: [...state.todos, newTodo],
+                length: state.todos.length + 1,
+                pending: state.todos.length + 1
+            };
+        }
+
+        case 'DELETE_TODO': {
+            const currentTodos = state.todos.filter((todo) => todo.id != action.payload);
+
+            return {
+                ...state,
+                todos: currentTodos,
+                length: currentTodos.length - 1,
+                completed: currentTodos.filter(todo => todo.completed).length,
+                pending: currentTodos.filter(todo => !todo.completed).length
+            };
+        }
+
+        case 'TOGGLE_TODO': {
+            const updatedTodo = state.todos.map(todo => {
+                if (todo.id === action.payload) {
+                    return { ...todo, completed: !todo.completed }
+                }
+
+                return todo;
+            });
+
+            return {
+                ...state,
+                todos: updatedTodo,
+                completed: updatedTodo.filter(todo => todo.completed).length,
+                pending: updatedTodo.filter(todo => !todo.completed).length
+            };
+        }
+
+        default:
+            return state;
+    }
+};
