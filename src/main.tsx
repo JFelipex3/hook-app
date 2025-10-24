34abdfc4c12547f81962a545c5d9a687f2d0ccfa
JFelipex3 <jfelipe.machuca@gmail.com>
2025-10-23 23:01:34 -0300

    feat: agregar componente ScrambleWords y lógica de juego con confetti

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './index.css'
 // import { PokemonPage } from './03-examples/PokemonPage';
 // import { FocusScreen } from './04-useRef/focusScreen';
 import { TasksApp } from './05-useReducer/TaskApp';
+import { ScrambleWords } from './05-useReducer/ScrambleWords';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
     {/* <FocusScreen /> */}
-    <TasksApp />
+    {/* <TasksApp /> */}
+    <ScrambleWords />
   </StrictMode>
 )
