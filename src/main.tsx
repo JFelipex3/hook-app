a65fdedab4dc5546344705f7c7f848765e581c3f
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-10-27 18:08:53 -0300

    feat: agregar componente MemoCounter y actualizar la importación en main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { MemoHook } from './06-memos/MemoHook'
+import { MemoCounter } from './06-memos/MemoCounter'
+// import { MemoHook } from './06-memos/MemoHook'
 // import { TrafficLight } from './01-useState/TrafficLight';
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
 // import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
@@ -20,6 +21,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <FocusScreen /> */}
     {/* <TasksApp /> */}
     {/* <ScrambleWords /> */}
-    <MemoHook />
+    {/* <MemoHook /> */}
+    <MemoCounter />
   </StrictMode>
 )
