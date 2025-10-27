0f7de7023bce80d68ff08c642dfb739d69b68018
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-10-27 18:08:43 -0300

    feat: agregar lógica para llamar a la API desde MySubTitle y optimizar el componente

--- src/06-memos/MemoHook.tsx
+++ src/06-memos/MemoHook.tsx
@@ -1,17 +1,23 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 import { MyTitle } from './ui/MyTitle';
 import { MySubTitle } from './ui/MySubTitle';
+
 export const MemoHook = () => {
 
   const [title, setTitle] = useState('Hola');
   const [subtitle, setSubTitle] = useState('Mundo');
+  
+  // Si se declara la función fuera del componente no es necesario usar el useCallback
+  const handleMyAPICall = useCallback( () => {
+    console.log('Llamar a mi API - ', subtitle);
+  }, [subtitle]);
     
   return (
     <div className="bg-gradient flex flex-col gap-4">
         <h1 className="text-2xl font-thin text-white">MemoApp</h1>
 
         <MyTitle title={title}/>
-        <MySubTitle subtitle={subtitle} />
+        <MySubTitle subtitle={subtitle} callMyAPI={handleMyAPICall}/>
 
         <button className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
             onClick={() => setTitle('Hello, ' + new Date().getTime())}
--- src/06-memos/ui/MySubTitle.tsx
+++ src/06-memos/ui/MySubTitle.tsx
@@ -2,9 +2,10 @@ import React from "react";
 
 interface Props {
     subtitle: string;
+    callMyAPI: () => void
 };
 
-export const MySubTitle = React.memo(({subtitle}: Props) => {
+export const MySubTitle = React.memo(({subtitle, callMyAPI}: Props) => {
 
     console.log('MySubTitle Re-render');
 
@@ -12,7 +13,11 @@ export const MySubTitle = React.memo(({subtitle}: Props) => {
         <>
             <h6 className="text-2xl font-bold">{subtitle}</h6>
 
-            <button className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer">Llamar a función</button>
+            <button className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer"
+                onClick={callMyAPI}
+            >
+                Llamar a función
+            </button>
         </>
     );
 });
