925591bf79d647c29e525a53807179b3868fe553
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-10-28 16:33:11 -0300

    feat: agregar componente Label y actualizar ProfilePage para usarlo

--- src/09-useContext/pages/profile/ProfilePage.tsx
+++ src/09-useContext/pages/profile/ProfilePage.tsx
@@ -1,6 +1,8 @@
 import { UserContext } from "@/09-useContext/context/UserContext";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { use, useContext } from "react"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import { Label } from "@radix-ui/react-label";
 
 export const ProfilePage = () => {
 
@@ -15,9 +17,22 @@ export const ProfilePage = () => {
       <h1 className="text-4xl">Perfil del usuario</h1>
       <hr />
 
-      <pre className="my-4 w-[80%] overflow-x-auto">
-        {JSON.stringify(user, null, 2)}
-      </pre>
+      <Card className="w-full max-w-screen">
+        <CardHeader>
+          <CardTitle>{user?.name}</CardTitle>
+          <CardDescription>{user?.title}</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-2">
+                <Label>Acerca de mi: {user?.about}</Label>
+                <Label>Skills: {user?.skills.join(" - ")}</Label>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
 
       <Button variant={"destructive"} onClick={logout}>Salir</Button>
     </div>
