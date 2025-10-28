a52018eed5643c2e40b861b7a7273e7eba5490df
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-10-28 15:09:23 -0300

    feat: mejorar manejo de autenticación y usuario en UserContext, LoginPage y ProfilePage

--- src/09-useContext/context/UserContext.tsx
+++ src/09-useContext/context/UserContext.tsx
@@ -1,5 +1,5 @@
-import { createContext, PropsWithChildren, useState } from "react";
-import { User } from '../data/user-mock.data';
+import { createContext, PropsWithChildren, useEffect, useState } from "react";
+import { User, users } from '../data/user-mock.data';
 
 type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
 
@@ -21,19 +21,47 @@ export const UserContextProvider = ({ children }: PropsWithChildren) => {
     const [user, setUser] = useState<User | null>(null);
 
     const handleLogin = (userId: number) => {
-        console.log({userId});
+        
+        const user = users.find(user => user.id === userId);
+
+        if(!user) {
+            console.log(`User not found ${userId}`);
+            setUser(null);
+            setAuthStatus('not-authenticated');
+            
+            return false;
+        }
+
+        setUser(user);
+        setAuthStatus('authenticated');
+        localStorage.setItem('userId', userId.toString());
+
         return true;
     }
 
     const handleLogout = () => {
-        console.log('logout');
+        setAuthStatus('not-authenticated');
+        setUser(null);
+        localStorage.removeItem('userId');
     }
+
+    useEffect( () => {
+        const storesUserId = localStorage.getItem('userId');
+
+        if (storesUserId) {
+            handleLogin(Number(storesUserId));
+            return;
+        }
+
+        handleLogout();
+    }, []);
 
     return (
         <UserContext value={
             {
-                authStatus: 'checking',
-                user: null,
+                authStatus: authStatus,
+                user: user,
+
                 login: handleLogin,
                 logout: handleLogout
             }
--- src/09-useContext/pages/profile/ProfilePage.tsx
+++ src/09-useContext/pages/profile/ProfilePage.tsx
@@ -1,16 +1,25 @@
+import { UserContext } from "@/09-useContext/context/UserContext";
 import { Button } from "@/components/ui/button"
+import { use, useContext } from "react"
 
 export const ProfilePage = () => {
+
+  // Usando el useContext
+  //const {user, logout} = useContext(UserContext);
+
+  // Usando API use()
+  const {user, logout} = use(UserContext);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl">Perfil del usuario</h1>
       <hr />
 
-      <pre className="my-4">
-        {JSON.stringify({}, null, 2)}
+      <pre className="my-4 w-[80%] overflow-x-auto">
+        {JSON.stringify(user, null, 2)}
       </pre>
 
-      <Button variant={"destructive"}>Salir</Button>
+      <Button variant={"destructive"} onClick={logout}>Salir</Button>
     </div>
   )
 }
